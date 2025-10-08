790471730484b8bda0d7c9cbc449377eea1f6131
cpainagent007 <cpainagent007@gmail.com>
2025-10-08 10:14:53 -0600

    Goodbye World Assignment

--- Source/Engine/Renderer/Renderer.h
+++ Source/Engine/Renderer/Renderer.h
@@ -3,6 +3,7 @@
 #include <SDL3_ttf/SDL_ttf.h>
 #include <SDL3_image/SDL_image.h>
 #include <iostream>
+#include <glad/glad.h>
 
 namespace neu {
 	/// <summary>
@@ -13,6 +14,7 @@ namespace neu {
 	/// </summary>
 	class Renderer
 	{
+		SDL_GLContext m_context;
 	public:
 		Renderer() = default;
 
